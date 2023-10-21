28f6d1a8275e9bb7ce94ab1b5798893480292467
Sawyer <git@sawyerh.com>
2023-10-21 15:35:48 -0700

    Fix glob pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./content/**/*.{mdx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./content/**/*.mdx"],
   theme: {
     extend: {
       colors: {
