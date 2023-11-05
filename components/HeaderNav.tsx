c1ff924ff937cf318e0cf2e117bee556bbe27c0d
Sawyer <git@sawyerh.com>
2023-11-05 09:17:25 -0800

    Show nav

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -2,8 +2,6 @@ import classNames from "classnames";
 import Link from "next/link";
 
 export function HeaderNav(props: { className?: string }) {
-  const showNav = process.env.NODE_ENV === "development";
-
   return (
     <header className={classNames(props.className, "mb-md")}>
       <nav>
@@ -11,23 +9,22 @@ export function HeaderNav(props: { className?: string }) {
           href="/"
           className="mr-4 inline-block py-4 font-semibold no-underline"
         >
-          Sawyer Hollenshead {showNav && <span className="font-marker">→</span>}
+          Sawyer Hollenshead <span className="font-marker">→</span>
         </Link>
-        {showNav &&
-          [
-            {
-              href: "/blog",
-              label: "Posts",
-            },
-          ].map(({ href, label }) => (
-            <Link
-              className="inline-block px-2 py-4 text-slate-700"
-              href={href}
-              key={href}
-            >
-              {label}
-            </Link>
-          ))}
+        {[
+          {
+            href: "/blog",
+            label: "Posts",
+          },
+        ].map(({ href, label }) => (
+          <Link
+            className="inline-block px-2 py-4 text-slate-700"
+            href={href}
+            key={href}
+          >
+            {label}
+          </Link>
+        ))}
       </nav>
     </header>
   );
